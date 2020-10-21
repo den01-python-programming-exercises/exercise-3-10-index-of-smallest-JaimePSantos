4e04f4a69f717ef96afbcf2574f2a347cca86562
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 21:28:34 +0000

    i think this is correct, weird error.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
+    intList = []
+    min1 = 9999999
+    index = 0
 
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+      if(min1>=intList[index]):
+        min1 = intList[index]
+      index+=1
+
+    
+    print("Smallest number: %s"%min1)
+
+    for i in range(len(intList)):
+      if(intList[i]==min1):
+        print("Found at index: %s"%i)
 if __name__ == '__main__':
     main()
