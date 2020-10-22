daa72c84944a48fe2d8f94da28c76b53a23c7cbf
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 11:05:27 +0000

    trying string list

--- src/exercise.py
+++ src/exercise.py
@@ -1,22 +1,23 @@
 def main():
     #write your code below this line
     intList = []
-    min1 = 9999999
+    min1 = str(9999999)
     index = 0
 
     while(True):
-      number = int(input())
-      if(number==-1):
+      numberStr = input()
+      if(numberStr=="-1"):
         break
-      intList.append(number)
+      intList.append(numberStr)
     
     for i in range(len(intList)):
-      if(intList[i]<min1):
+      if(int(intList[i])<int(min1)):
         min1 = intList[i]
     print("Smallest number: %s"%min1)
 
     for i in range(len(intList)):
-      if(intList[i]==min1):
+      if(int(intList[i])==int(min1)):
         print("Found at index: %s"%i)
+    
 if __name__ == '__main__':
     main()
